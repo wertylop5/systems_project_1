8ce3bf98b0e33c8223da6de685d1354abe9f5302
Stanley Lin <slin55055@gmail.com>
2017-11-22 15:29:47 -0500

    stuff

--- src/shell.c
+++ src/shell.c
@@ -11,13 +11,22 @@ if cd:
 if command == exit
 	exit(0)
 */
+#include<string.h>
+
 #include"include/shell.h"
 
 char* read_line() {
 	return 0;
 }
 
+/*
+Will split command string on spaces
+*/
 char** parse_args(char *line) {
+	char **result;
+	
+	line = strsep(result, ";")
+	
 	return 0;
 }
 
